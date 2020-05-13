2fc510c010b9c0067aced37964e32bd94a19b035
Žydrūnas Pavinkšnis <63241045+zydrunas-pavinksnis@users.noreply.github.com>
2020-05-13 10:40:13 +0300

    apjungta

--- js/bartender-drinks.js
+++ js/bartender-drinks.js
@@ -115,3 +115,5 @@ function zeroFuel(distanceToPump, mpg, fuelLeft) {
 
 console.log(zeroFuel(50, 25, 2), true);
 console.log(zeroFuel(100, 50, 1), false);
+
+console.log(`----------------------------------------`);
